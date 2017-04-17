5bd5ae42093418877f311bf1d8af170b7ffbdb94
Jeff Villarina <jeffvilla@Jeffreys-MacBook-Pro.local>
2017-04-17 16:22:30 -0700

    (Feature) Intgrated react, sass & webpack

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../sass/styles.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='example'>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const ProcessEnvPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('development')
+  }
+})
+
+const SassPlugin = new ExtractTextPlugin({
+  filename: './styles/main.css'
+});
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!sass-loader') }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, SassPlugin, new webpack.HotModuleReplacementPlugin()] // add ProcessEnvPlugin if not working
+};
