e2d7fbd890668381b1d5483508c2725568f13915
Jeff Villarina <jeffvilla@Jeffreys-MacBook-Pro.local>
2017-05-03 00:41:47 -0700

    (Enhancement) Add portfolio images to assets

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -7,7 +7,7 @@ export default class Profile extends React.Component {
 
   render() {
     return (
-      <div className='l-row profile'>
+      <div className='l-row l-col-12 profile'>
         <ProfilePicture />
         <ProfileInfo />
       </div>
